59324cb02725aaf58089050aecb34296317ac7d5
Sixin Yang <sixin.yang@lumentum.com>
2025-06-05 16:55:02 +0800

    fix issue for creating debug version of devLinux

--- mk_debug.sh
+++ mk_debug.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Add this file into build system to make program and libraries compiled just with debug_info compared with normal build.
 # Use the following command to make with debug info
 # example: output_style=full JDSU_POSTBUILD=../toolsBuildScripts/target_os/qnx/mk_debug.sh CCFLAGS=-g make -j16 JDSU_PRODUCT=rla1x5 TOOLCHAIN=dra821 load 2>&1 | tee build.opt-dbg.log
 
 OBJSTRIP=$QNX_HOST/usr/bin/aarch64-unknown-nto-qnx7.1.0-strip
+READELF=$QNX_HOST/usr/bin/aarch64-unknown-nto-qnx7.1.0-readelf
 
 name=$1
 src=$2
 dst=${src%/rootfs/*}/debug_files/${src#*/rootfs/}
+
+# check if the source file is already stripped
+$READELF -S $src | grep debug_info
+if (( $? != 0 )); then
+    echo "$src had been stripped"
+    exit 0
+fi
 
 # convert lib%.so to %.1
 fn=$(basename $dst)
